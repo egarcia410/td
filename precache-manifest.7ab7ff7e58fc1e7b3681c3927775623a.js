f01735e42b6556ab86190c6bd5b5791b86abad79
Emmanuel Garcia <egarcia@Emmanuels-MacBook-Pro.local>
2020-05-26 16:12:04 -0400

    Updates

---
+++ precache-manifest.7ab7ff7e58fc1e7b3681c3927775623a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d0b888ef0ff99fb6dcb93a0b82d039",
+    "url": "/td/index.html"
+  },
+  {
+    "revision": "6acba01e7bf257e2f4fd",
+    "url": "/td/static/js/2.4556c79b.chunk.js"
+  },
+  {
+    "revision": "ac5edef27f6d8744e8c73a8e2ef3da18",
+    "url": "/td/static/js/2.4556c79b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "866ea8368f350f27437c",
+    "url": "/td/static/js/main.1904a98c.chunk.js"
+  },
+  {
+    "revision": "24acb6eac7e63816cbfa",
+    "url": "/td/static/js/runtime-main.830cadd0.js"
+  },
+  {
+    "revision": "cdbba6c82c6a6f95d8ce77ec28291159",
+    "url": "/td/static/media/Boulder_Badge.cdbba6c8.png"
+  },
+  {
+    "revision": "56a6ec883c1da2891344ddae97d6df26",
+    "url": "/td/static/media/Cascade_Badge.56a6ec88.png"
+  },
+  {
+    "revision": "3f8f90d8f0e1a5fa4a662c6c3a339d1b",
+    "url": "/td/static/media/Earth_Badge.3f8f90d8.png"
+  },
+  {
+    "revision": "9fd8d94631109441ff8a95de86760cfc",
+    "url": "/td/static/media/Marsh_Badge.9fd8d946.png"
+  },
+  {
+    "revision": "82f8f7cd05afe91a7ce4d4b6c364c902",
+    "url": "/td/static/media/Rainbow_Badge.82f8f7cd.png"
+  },
+  {
+    "revision": "7ccd9b5b2a2caecea4bd8c5284ea2e2c",
+    "url": "/td/static/media/Soul_Badge.7ccd9b5b.png"
+  },
+  {
+    "revision": "bb14b6f3396fc7d2ba0740bbbc2690e1",
+    "url": "/td/static/media/Thunder_Badge.bb14b6f3.png"
+  },
+  {
+    "revision": "bf2de797c8d015468f239ec8f0737f2b",
+    "url": "/td/static/media/Volcano_Badge.bf2de797.png"
+  }
+]);
